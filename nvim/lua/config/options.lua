8522a0c6abd5f75fa765ef1bfd933219cd46dcf8
Muhammad Ghazali <152017206+ELghazX@users.noreply.github.com>
2025-01-23 17:40:55 +0800

    Add: Neovim configuration
    Minimalist UI workspace neovim with Lazyvim based

---
+++ nvim/lua/config/options.lua
@@ -0,0 +1,10 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+-- Set tab and indentation to 4 spaces
+vim.opt.tabstop = 4 -- Number of spaces a <Tab> represents
+vim.opt.shiftwidth = 4 -- Number of spaces to use for autoindent
+vim.opt.softtabstop = 4 -- Number of spaces a <Tab> counts for while editing
+vim.opt.expandtab = true -- Convert tabs to spaces
+-- Set to `false` to globally disable all snacks animations
+vim.g.snacks_animate = false
